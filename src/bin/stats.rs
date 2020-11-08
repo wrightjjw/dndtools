dffe28cd65a632fe5f230b019108056b3e4205f4
Josh Wright <wright.jjw@gmail.com>
2020-11-07 19:19:14 -0500

    stats bin

--- src/bin/stats.rs
+++ src/bin/stats.rs
@@ -1,5 +1,15 @@
+use dndtools::gen_stats;
 use std::env;
+
+fn run(args: Vec<String>) {
+    let stats = gen_stats();
+    for s in stats.iter() {
+        print!("{} ", s);
+    }
+    print!("\n");
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    run(args);
 }
