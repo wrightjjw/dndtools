db3566c4ab92a22af58267570eab821d8f2c8d3f
Josh Wright <wright.jjw@gmail.com>
2020-11-15 12:55:13 -0500

    help message

--- src/bin/stats.rs
+++ src/bin/stats.rs
@@ -4,11 +4,18 @@ use std::env;
 
 fn run(args: Vec<String>) {
     let mut opts = Options::new();
-    opts.optopt("n", "", "number of stat blocks to calculate", "NUM");
+    opts.optopt("n", "", "number of stat blocks to calculate", "[NUM]");
+    opts.optflag("h", "help", "display help information");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
         Err(e) => panic!(e.to_string()),
     };
+
+    if matches.opt_present("h") {
+        let brief = format!("Usage: {} [OPTIONS]", args[0]);
+        print!("{}", opts.usage(&brief));
+        return;
+    }
 
     // define num_rolls from n option, panic if error
     let num_rolls: u32 = match matches.opt_get_default("n", 1) {
