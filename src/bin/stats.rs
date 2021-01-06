c0c1aa3eddcd901d81b6dc22ecd5a48554d2a85d
Josh Wright <wright.jjw@gmail.com>
2021-01-06 16:51:16 -0600

    stats: beautiful error handling
    run() now returns a Result. All possible errors are now returned and
    handled by main(), which prints a concise error message and exits with
    code 1.
    This closes #15

--- src/bin/stats.rs
+++ src/bin/stats.rs
@@ -5,9 +5,10 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use std::process::exit;
 use std::sync::{Arc, Mutex};
 
-fn run(args: Vec<String>) {
+fn run(args: Vec<String>) -> Result<(), String> {
     let mut opts = Options::new();
     opts.optopt("n", "", "number of stat blocks to calculate", "[NUM]");
     opts.optopt("f", "file", "write output to file", "[FILE]");
@@ -16,13 +17,15 @@ fn run(args: Vec<String>) {
     opts.optflag("h", "help", "display help information");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
-        Err(e) => panic!(e.to_string()),
+        Err(e) => {
+            return Err(e.to_string());
+        }
     };
 
     if matches.opt_present("h") {
         let brief = format!("Usage: {} [OPTIONS]", args[0]);
         print!("{}", opts.usage(&brief));
-        return;
+        return Ok(());
     }
 
     // check if quiet flag is present
@@ -35,8 +38,10 @@ fn run(args: Vec<String>) {
 
     // define num_rolls from n option, panic if error
     let num_rolls: usize = match matches.opt_get_default("n", 1) {
-        Ok(m) => m,
-        Err(e) => panic!(e.to_string()),
+        Ok(n) => n,
+        Err(e) => {
+            return Err("-n: ".to_string() + &e.to_string());
+        }
     };
 
     // open a file with mutex, if necessary
@@ -49,38 +54,48 @@ fn run(args: Vec<String>) {
     // define num_threads from j option, panic if error
     let num_threads = match matches.opt_get_default("j", num_cpus::get()) {
         Ok(m) => m,
-        Err(e) => panic!(e.to_string()),
+        Err(e) => {
+            return Err("-j: ".to_string() + &e.to_string());
+        }
     };
     rayon::ThreadPoolBuilder::new()
         .num_threads(num_threads)
         .build_global()
-        .unwrap();
+        .map_err(|e| e.to_string())?;
 
-    (0..num_rolls).into_par_iter().for_each(|_x| {
-        let stats = gen_stats();
-        // println!("Thread {} here.", match rayon::current_thread_index() {
-        //     Some(x) => x,
-        //     None => 0,
-        // });
-        let out = format!(
-            "{} {} {} {} {} {}\n",
-            stats[5], stats[4], stats[3], stats[2], stats[1], stats[0]
-        );
-        if file_mode {
-            file.lock()
-                .unwrap()
-                .as_ref()
-                .unwrap()
-                .write_all(out.as_bytes())
-                .unwrap();
-        }
-        if !quiet {
-            print!("{}", out);
-        }
-    });
+    (0..num_rolls)
+        .into_par_iter()
+        .try_for_each(|_| -> Result<(), String> {
+            let stats = gen_stats();
+            let out = format!(
+                "{} {} {} {} {} {}\n",
+                stats[5], stats[4], stats[3], stats[2], stats[1], stats[0]
+            );
+            if file_mode {
+                file.lock()
+                    .map_err(|e| e.to_string())?
+                    .as_ref()
+                    .map_err(|e| e.to_string())?
+                    .write_all(out.as_bytes())
+                    .map_err(|e| e.to_string())?
+            }
+            if !quiet {
+                print!("{}", out);
+            }
+            Ok(())
+        })?;
+
+    Ok(())
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    run(args);
+    match run(args) {
+        Ok(_) => exit(0),
+        Err(e) => {
+            println!("stats: {}", e);
+            println!("Try 'stats --help' for more info.");
+            exit(1);
+        }
+    };
 }
